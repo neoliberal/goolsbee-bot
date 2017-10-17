da9f5e640028bc4abf8435d2d2c744c399cb2d5e
Abhi Agarwal <goldcobrapro@gmail.com>
2017-10-17 10:00:27 -0400

    MyPy compliant

--- source/goolsbee.py
+++ source/goolsbee.py
@@ -1,56 +1,66 @@
-#!/usr/bin/python
-import praw
+#!/usr/bin/python3.6
+"""not stupid"""
 import random
+from typing import List
 
-reddit = praw.Reddit('Goolsbee')
-responses = open("Replies.txt").read().split("\n")
-commented = open("replied_comments.txt").read().split()
+import praw
 
-def clean_text(s):
-    punc_list = [".", ";", ":", "!", "?", "/", "\\", ",", "#", "@", "$", "&", ")", "(", "'", ">", "[", "]"]
-    new_s = ''
-    for i in s:
+
+def clean_text(text: str) -> str:
+    """clean text to remove punctuation"""
+    punc_list: List[str] = [".", ";", ":", "!", "?", "/", "\\", ",",
+                            "#", "@", "$", "&", ")", "(", "'", ">", "[", "]"]
+    new_s: str = ''
+    for i in text:
         if i not in punc_list:
             new_s += i
         else:
             new_s += ' '
     return new_s.lower()
 
-def log_comment(comment):
+
+def log_comment(comment: praw.models.Comment) -> None:
+    """log comment to file"""
     with open('replied_comments.txt', 'a') as file:
         file.write(str(comment))
         file.write(' ')
 
-def write_comment(i, comment):
-    response = responses[i]
+
+def write_comment(i: int, comment: praw.models.Comment) -> None:
+    """writes comment to user"""
+    responses: List[str] = open("Replies.txt").read().split("\n")
+    response: str = responses[i]
     log_comment(comment)
-    if(random.randint(1,11)==1):
+    if random.randint(1, 11) == 1:
         comment.reply(response)
         print('Comment written')
     else:
         print('Comment passed')
-    
 
-while True:
-    for comment in reddit.subreddit('neoliberal').stream.comments():
-        if not comment.author == "Goolsbee_Bot":
-                text = comment.body
-                text = clean_text(text)
-                text = text.split()
+
+def goolsbee() -> None:
+    """main goolsbee"""
+    reddit: praw.Reddit = praw.Reddit('Goolsbee')
+    commented = open("replied_comments.txt").read().split()
+
+    while True:
+        for comment in reddit.subreddit('neoliberal').stream.comments():
+            if not comment.author == "Goolsbee_Bot":
+                text: List[str] = clean_text(comment.body).split()
                 if str(comment) not in commented:
-            
+
                     if 'bitcoin' in text:
                         write_comment(16, comment)
 
                     elif 'anime' in text:
                         write_comment(5, comment)
-                        
+
                     elif 'democracy' in text:
                         write_comment(27, comment)
-                    
+
                     elif 'stupid' in text:
                         write_comment(26, comment)
-                        
+
                     elif 'tax' in text and 'cuts' in text:
                         write_comment(20, comment)
 
@@ -68,10 +78,14 @@ while True:
 
                     elif 'bernie' in text and 'would' in text and 'have' in text and 'won':
                         write_comment(25, comment)
-                        
+
                     elif 'coal' in text and 'miners' in text:
                         write_comment(2, comment)
-                
+
                     elif 'goolsbee' in text:
-                        rint = random.randint(0,24)
+                        rint: int = random.randint(0, 24)
                         write_comment(rint, comment)
+
+
+if __name__ == '__main__':
+    goolsbee()
