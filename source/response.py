96b5faa02b3cf4838b2d9ac6a5394ee02226f06b
Abhi Agarwal <goldcobrapro@gmail.com>
2017-10-18 10:57:13 -0400

    Add Custom JSON decoder

--- source/response.py
+++ source/response.py
@@ -1,5 +1,6 @@
 """class for handling possible goolsbee responses"""
-from typing import List
+import json
+from typing import List, Dict, Any
 
 
 class Response(object):
@@ -17,3 +18,13 @@ class Response(object):
     def __str__(self: Response) -> str:
         """returns formatted link"""
         return "[{1}]({2})".format(self.text, self.image)
+
+
+class ResponseDecoder(json.JSONDecoder):
+    """decodes json into Response"""
+    def __init__(self: ResponseDecoder) -> None:
+        json.JSONDecoder.__init__(self, object_hook=self.object_dict)
+
+    def object_dict(self: ResponseDecoder, obj: Dict[str, Any]) -> Response:
+        """returns Response from dictionary"""
+        return Response(obj["image"], obj["text"], obj["triggers"])
