dd6c589e80b17067a7d51f53dafa9e53770be155
Abhi Agarwal <goldcobrapro@gmail.com>
2017-10-18 10:37:54 -0400

    Add responses

---
+++ source/response.py
@@ -0,0 +1,19 @@
+"""class for handling possible goolsbee responses"""
+from typing import List
+
+
+class Response(object):
+    """hold response data"""
+
+    def __init__(self: Response, image: str, text: str, triggers: List[str]) -> None:
+        self.image = image
+        self.text = text
+        self.triggers = triggers
+
+    def has_word(self: Response, words: List[str]) -> bool:
+        """check if any words are in trigger"""
+        return any((True for word in words if word in self.triggers))
+
+    def __str__(self: Response) -> str:
+        """returns formatted link"""
+        return "[{1}]({2})".format(self.text, self.image)
