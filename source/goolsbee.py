a03104c0965c539f0ca6dddee8d4359326ac11a3
Abhi Agarwal <goldcobrapro@gmail.com>
2017-10-21 19:48:11 -0400

    Update checking logic to utilize triggers
    Along with string comprehension this creates minimal code

--- source/goolsbee.py
+++ source/goolsbee.py
@@ -2,7 +2,7 @@
 """not stupid"""
 import json
 import random
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Optional
 import string
 
 import praw
@@ -20,7 +20,7 @@ class Goolsbot(object):
             res_list: List[Dict[str, Any]] = json.loads(responses_file.read())["responses"]
             self.responses: List[Response] = [
                 Response(item["image"], item["text"], item["triggers"]) for item in res_list
-                ]
+            ]
 
         while True:
             self.run()
@@ -30,65 +30,28 @@ class Goolsbot(object):
         punc_remover = str.maketrans('', '', string.punctuation)
         for comment in self.reddit.subreddit('neoliberal').stream.comments():
             if not comment.author == self.reddit.user.me():
+                # roll the dice, currently 5% of goolsbot posting
+                chance: bool = random.randint(1, 21) == 1
                 text: List[str] = str(comment.body).translate(punc_remover).lower().split()
-                if str(comment) not in self.commented:
-
-                    if 'bitcoin' in text:
-                        self.write_comment(16, comment)
-
-                    elif 'anime' in text:
-                        self.write_comment(5, comment)
-
-                    elif 'democracy' in text:
-                        self.write_comment(27, comment)
-
-                    elif 'stupid' in text:
-                        self.write_comment(26, comment)
-
-                    elif 'tax' in text and 'cuts' in text:
-                        self.write_comment(20, comment)
-
-                    elif 'cut' in text and 'taxes' in text:
-                        self.write_comment(20, comment)
-
-                    elif 'cutting' in text and 'taxes' in text:
-                        self.write_comment(20, comment)
-
-                    elif 'tea' in text and 'party' in text:
-                        self.write_comment(20, comment)
-
-                    elif 'gold' in text and 'standard' in text:
-                        self.write_comment(0, comment)
-
-                    elif 'bernie' in text and 'would' in text and 'have' in text and 'won':
-                        self.write_comment(25, comment)
-
-                    elif 'coal' in text and 'miners' in text:
-                        self.write_comment(2, comment)
-
+                if str(comment) not in self.commented and chance:
+                    combos: Optional[List[Response]] = [
+                        response for response in self.responses for
+                        word in text if word in response.triggers
+                    ]
+                    if combos:
+                        self.write_comment(random.choice(combos), comment)
                     elif 'goolsbee' in text:
-                        rint: int = random.randint(0, 24)
-                        self.write_comment(rint, comment)
+                        self.write_comment(
+                            random.choice(self.responses), comment)
 
-    def write_comment(self: Goolsbot, i: int, comment: praw.models.Comment) -> None:
-        """writes comment to user"""
-        responses: List[str] = open("Replies.txt").read().split("\n")
-        response: str = responses[i]
-        self.log_comment(comment)
-        if random.randint(1, 11) == 1:
-            comment.reply(response)
-            print('Comment written')
-            return
-
-        print('Comment passed')
-        return
-
-    def log_comment(self: Goolsbot, comment: praw.models.Comment) -> None:
-        """log comment to file"""
+    def write_comment(self: Goolsbot, response: Response, comment: praw.models.Comment) -> None:
+        """log comment and writes to file"""
         with open('replied_comments.txt', 'a') as file:
             file.write(str(comment))
             file.write(' ')
 
+        comment.reply(str(response))
+        print('Comment written')
 
 if __name__ == '__main__':
     Goolsbot()
