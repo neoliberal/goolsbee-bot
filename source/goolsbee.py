e738b72473341ca7aef9a360215c23c0b29b09b6
Abhi Agarwal <goldcobrapro@gmail.com>
2017-10-21 20:48:51 -0400

    Get rid of one level of JSON

--- source/goolsbee.py
+++ source/goolsbee.py
@@ -19,7 +19,7 @@ class Goolsbot(object):
             self.commented: List[str] = replied.read().split()
 
         with open("data/responses.json") as responses:
-            res_list: List[Dict[str, Any]] = json.loads(responses.read())["responses"]
+            res_list: List[Dict[str, Any]] = json.loads(responses.read())
             self.responses: List[Response] = [
                 Response(item["image"], item["text"], item["triggers"]) for item in res_list
             ]
