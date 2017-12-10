341e7853203b775c9f8ca1a20d2b61d19259bf80
Abhi Agarwal <goldenercobra@gmail.com>
2017-12-10 10:11:01 -0500

    Logging

--- goolsbee_bot/goolsbee.py
+++ goolsbee_bot/goolsbee.py
@@ -5,24 +5,41 @@ import random
 from typing import List, Dict, Any
 
 import praw
+from slack_python_logging import slack_logger
 
 from .response import Response
-
 
 class Goolsbot(object):
     """goolsbot class"""
 
     def __init__(self: Goolsbot, reddit: praw.Reddit, subreddits: List[str]) -> None:
+        self.logger = slack_logger.initialize("goolsbee-bot")
+        self.logger.debug("Intializing")
+
+        self.logger.debug("Logging into reddit")
         self.reddit: praw.Reddit = reddit
-        self.subreddits: List[str] = subreddits
+        self.logger.debug("Logged into reddit")
+
+        self.logger.debug("Validating subreddits")
+        self.subreddits: List[praw.models.Subreddit] = []
+        for subreddit in subreddits:
+            try:
+                self.subreddits.append(self.reddit.subreddit(subreddit))
+            except praw.exceptions.APIException:
+                self.logger.warning("\"%s\" is an invalid subreddit, skipping", subreddit)
+        self.logger.debug("Validated subreddits")
+
         with open("replied_comments.txt") as replied:
             self.commented: List[str] = replied.read().split()
 
+        self.logger.debug("Opening responses")
         with open("data/responses.json") as responses:
             res_list: List[Dict[str, Any]] = json.loads(responses.read())
             self.responses: List[Response] = [
                 Response(item["image"], item["text"], item["triggers"]) for item in res_list
             ]
+        self.logger.debug("Opened responses")
+        self.logger.info("goolsbee-bot intialized successfully")
         return
 
     import string
@@ -30,14 +47,7 @@ class Goolsbot(object):
     def run(self: Goolsbot) -> None:
         """run the main code"""
         for subreddit in self.subreddits:
-            validated: praw.models.Subreddit = self.reddit.subreddit()
-            try:
-                validated = self.reddit.subreddit(subreddit)
-            except praw.exceptions.APIException as praw_error:
-                print(praw_error)
-                continue
-
-            for comment in validated.stream.comments():
+            for comment in subreddit.stream.comments():
                 if not comment.author == self.reddit.user.me():
                     if str(comment) not in self.commented:
                         text: List[str] = comment.body.translate(self.punc_remover).lower().split()
@@ -47,9 +57,10 @@ class Goolsbot(object):
                         ]
                         if combos:
                             self.write_comment(random.choice(combos), comment)
-                        elif 'goolsbee' in text:
+                        elif "goolsbee" in text:
                             self.write_comment(
-                                random.choice(self.responses), comment)
+                                random.choice(self.responses), comment
+                            )
 
     # pylint: disable=R0201
     def write_comment(self: Goolsbot, response: Response, comment: praw.models.Comment) -> None:
