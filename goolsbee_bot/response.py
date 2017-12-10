6d19a5a6e8564a42451930ff0bb3e1bdbb091f52
Abhi Agarwal <goldenercobra@gmail.com>
2017-12-10 10:59:25 -0500

    Convert links into tables

--- goolsbee_bot/response.py
+++ goolsbee_bot/response.py
@@ -1,18 +1,39 @@
 """class for holding possible goolsbee responses"""
-from typing import List
+from typing import List, Dict, Any
+
+from tabulate import tabulate
 
 # pylint: disable=R0903
 class Response(object):
     """hold response data"""
 
-    def __init__(self: Response, image: str, text: str, triggers: List[str]) -> None:
-        self.image = image
-        self.text = text
-        self.triggers = triggers
+    def __init__(self: Response, triggers: List[str], table: Dict[str, str]) -> None:
+        self.triggers: List[str] = triggers
+        self.table: Dict[str, Any] = table
 
     def __str__(self: Response) -> str:
-        """returns formatted link"""
-        return f"[{self.text}]({self.image})"
+        """returns formatted table"""
+        def vote(level: int) -> str:
+            """converts voting number into string"""
+            vote_dict: Dict[int, str] = {
+                -2: "Strongly Disagree",
+                -1: "Disagree",
+                0: "Uncertain",
+                1: "Agree",
+                2: "Strongly Agree"
+            }
+            return vote_dict[level]
+
+        return tabulate(
+            [[
+                vote(self.table["vote"]),
+                self.table["confidence"],
+                self.table["comment"]
+            ]],
+            headers=["Vote", "Confidence", "Comments"],
+            tablefmt="pipe",
+            numalign="left"
+        )
 
     def has_words(self: Response, words: List[str]) -> bool:
         """check if all words are found in trigger"""
